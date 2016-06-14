3d15f25be0a8a016530fd6bd282b59401a367c62
Douglas Davis <ddavis0926@outlook.com>
2016-06-14 14:18:26 -0500

    Add project files.

---
+++ FrontendWebRole/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FrontendWebRole.Models;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.ServiceBus;
+
+namespace FrontendWebRole.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("Submit");
+        }
+
+        public ActionResult About()
+        {
+            // ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Submit()
+        {
+            //Will put code for displaying queue message count here.
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            //ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        //POST: /Home/Submit.
+        // Controler method for a view you will create for the submission form
+        [HttpPost]
+        // Attribute to help prevent cross-site scripting attacks and
+        // cross-site request forgery.
+        [ValidateAntiForgeryToken]
+        public ActionResult Submit(OnlineOrders order)
+        {
+            if (ModelState.IsValid)
+            {
+                // Will put code for submitting to queue here.
+                return RedirectToAction("Submit");
+            }
+
+            else
+            {
+                return View(order);
+            }
+        }
+    }
+}
---
+++ FrontendWebRole/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FrontendWebRole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NA")]
+[assembly: AssemblyProduct("FrontendWebRole")]
+[assembly: AssemblyCopyright("Copyright © NA 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8c0134b8-e0e0-412d-9fff-25e6200d3d7d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
